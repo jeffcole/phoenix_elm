9bb4dacd50f16fc10e7f0dfba161f2613c2b2c9e
Jeff Cole <jeffreydonaldcole@gmail.com>
2016-05-18 05:22:29 -0600

    Remove username/password in database config

--- config/dev.exs
+++ config/dev.exs
@@ -35,8 +35,6 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :phoenix_elm, PhoenixElm.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "phoenix_elm_dev",
   hostname: "localhost",
   pool_size: 10
